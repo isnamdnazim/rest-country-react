2ffd49e1c340f00f605182f5e8a381f77f86f99d
isnamdnazim <isnamdnazim@gmail.com>
2021-09-20 01:08:11 +0600

    country total population showing in web page

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
     return (
-        <div>
+        <div className="country">
             <h2>Country Name: {props.name}</h2>
             <h4>Capital Name: {props.capital}</h4>
             <h5>Population: {props.population}</h5>
