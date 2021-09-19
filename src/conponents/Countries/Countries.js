b3ee76d2748e7af02d21f03c1ccf028ebed2c75c
isnamdnazim <isnamdnazim@gmail.com>
2021-09-20 00:16:00 +0600

    first commit

---
+++ src/conponents/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>This is created by react code snippet rsc</h1>
+        </div>
+    );
+};
+
+export default Countries;
