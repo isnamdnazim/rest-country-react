5915a443e7c4a4066b9bfadf0a87747e48f02e20
isnamdnazim <isnamdnazim@gmail.com>
2021-09-20 01:27:57 +0600

    country region showing in web page

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    console.log(props)
-    const { name, flag, capital, population } = props.country
+    console.log(props.country)
+    const { name, flag, capital, population, region } = props.country
     return (
         <div className="country">
             <img src={flag} alt="" />
             <h2>Country Name: {name}</h2>
             <h4>Capital Name: {capital}</h4>
             <h5>Population: {population}</h5>
+            <p>Region: {region}</p>
         </div>
     );
 };
